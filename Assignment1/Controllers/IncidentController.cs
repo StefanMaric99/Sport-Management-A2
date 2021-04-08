720df4a740af52382330d52d0ba90842f8ad0da6
ateleiotes <47301001+ateleiotes@users.noreply.github.com>
2021-04-08 17:31:46 -0400

    Added incident view model

--- Assignment1/Controllers/IncidentController.cs
+++ Assignment1/Controllers/IncidentController.cs
@@ -1,10 +1,12 @@
 ﻿using Assignment1.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+
+
 
 namespace Assignment1.Controllers
 {
@@ -19,64 +21,186 @@ namespace Assignment1.Controllers
         public IActionResult List()
         {
             // Returns list of incidents ordered by title
-            var incidents = context.Incidents
-                .Include(c => c.Customer)
-                .Include(p => p.Product)
-                .Include(t => t.Technician)
-                .OrderBy(context => context.Title).ToList();
-            return View(incidents);
+            IncidentsViewModel model = new IncidentsViewModel();
+            List<Incident> incidents = new List<Incident>();
+
+            IQueryable<Incident> incidentsQ = context.Incidents
+                                                         .Include(i => i.Customer)
+                                                         .Include(p => p.Product)
+                                                         .Include(t => t.Technician);
+
+            // add filtering
+
+            incidents = incidentsQ.ToList();
+            model.incidents = incidents;
+
+            return View(model);
         }
 
         [HttpGet]
         public IActionResult Add()
         {
-            ViewData["Message"] = "incidents page";
-            ViewBag.Action = "Add";
-            ViewBag.Customers = context.Customers.OrderBy(c => c.FirstName).ToList();
-            ViewBag.Products = context.Product.OrderBy(p => p.Name).ToList();
-            ViewBag.Technicians = context.Technicians.OrderBy(t => t.Name).ToList();
-            return View("Edit", new Incident());
+
+            List<SelectListItem> customers = context.Customers.ToList().ConvertAll(c => {
+                return new SelectListItem()
+                {
+                    Text = c.FirstName + " " + c.LastName,
+                    Value = c.CustomerId.ToString(),
+                    Selected = false
+
+                };
+            });
+
+            List<SelectListItem> products = context.Product.ToList().ConvertAll(p => {
+                return new SelectListItem()
+                {
+                    Text = p.Name,
+                    Value = p.ProductId.ToString(),
+                    Selected = false
+
+                };
+            });
+
+            List<SelectListItem> technicians = context.Technicians.ToList().ConvertAll(t => {
+                return new SelectListItem()
+                {
+                    Text = t.Name,
+                    Value = t.TechnicianId.ToString(),
+                    Selected = false
+                };
+            });
+            IncidentsViewModel model = new IncidentsViewModel
+            {
+                customers = customers,
+                products = products,
+                technicians = technicians,
+                currentIncident = new Incident(),
+                Action = "Edit"
+        };
+            
+
+
+            return View("Edit", model);
+            //ViewData["Message"] = "incidents page";
+            //ViewBag.Action = "Add";
+            //ViewBag.Customers = context.Customers.OrderBy(c => c.FirstName).ToList();
+            //ViewBag.Products = context.Product.OrderBy(p => p.Name).ToList();
+            //ViewBag.Technicians = context.Technicians.OrderBy(t => t.Name).ToList();
+            //return View("Edit", new Incident());
         }
         [HttpGet]
         public IActionResult Edit(int id)
         {
-            ViewBag.Action = "Edit";
-            ViewBag.Customers = context.Customers.OrderBy(c => c.FirstName).ToList();
-            ViewBag.Products = context.Product.OrderBy(p => p.Name).ToList();
-            ViewBag.Technicians = context.Technicians.OrderBy(t => t.Name).ToList();
-            var incident = context.Incidents.Include(c => c.Customer).Include(p => p.Product).Include(t =>t.Technician).FirstOrDefault(c => c.IncidentId == id);
-            return View(incident);
+            Incident incident = context.Incidents
+                                  .Include(c => c.Customer)
+                                  .Include(p => p.Product)
+                                  .Include(t => t.Technician)
+                                  .FirstOrDefault(i => i.IncidentId == id);
+
+            IncidentsViewModel model = new IncidentsViewModel();
+
+
+            List<SelectListItem> customers = context.Customers.ToList().ConvertAll(c => {
+                return new SelectListItem()
+                {
+                    Text = c.FirstName + " " + c.LastName,
+                    Value = c.CustomerId.ToString(),
+                    Selected = false
+
+                };
+            });
+
+            List<SelectListItem> products = context.Product.ToList().ConvertAll(p => {
+                return new SelectListItem()
+                {
+                    Text = p.Name,
+                    Value = p.ProductId.ToString(),
+                    Selected = false
+
+                };
+            });
+
+            List<SelectListItem> technicians = context.Technicians.ToList().ConvertAll(t => {
+                return new SelectListItem()
+                {
+                    Text = t.Name,
+                    Value = t.TechnicianId.ToString(),
+                    Selected = false
+                };
+            });
+
+
+            model.customers = customers;
+            model.products = products;
+            model.technicians = technicians;
+            model.currentIncident = incident;
+            model.Action = "Edit";
+
+            return View("Edit", model);
+            //ViewBag.Action = "Edit";
+            //ViewBag.Customers = context.Customers.OrderBy(c => c.FirstName).ToList();
+            //ViewBag.Products = context.Product.OrderBy(p => p.Name).ToList();
+            //ViewBag.Technicians = context.Technicians.OrderBy(t => t.Name).ToList();
+            //var incident = context.Incidents.Include(c => c.Customer).Include(p => p.Product).Include(t =>t.Technician).FirstOrDefault(c => c.IncidentId == id);
+            //return View(incident);
         }
         [HttpPost]
-        public IActionResult Edit(Incident incident)
-        {
-            string action = (incident.IncidentId == 0) ? "Add" : "Edit";
+        public IActionResult Edit(IncidentsViewModel model)
 
-            if (ModelState.IsValid)
-            {
-                if (action == "Add")
+        {                     
+
+            List<SelectListItem> customers = context.Customers.ToList().ConvertAll(c => {
+                return new SelectListItem()
                 {
+                    Text = c.FirstName + " " + c.LastName,
+                    Value = c.CustomerId.ToString(),
+                    Selected = false
 
-                    context.Incidents.Add(incident);
+                };
+            });
+
+            List<SelectListItem> products = context.Product.ToList().ConvertAll(p => {
+                return new SelectListItem()
+                {
+                    Text = p.Name,
+                    Value = p.ProductId.ToString(),
+                    Selected = false
+
+                };
+            });
+
+            List<SelectListItem> technicians = context.Technicians.ToList().ConvertAll(t => {
+                return new SelectListItem()
+                {
+                    Text = t.Name,
+                    Value = t.TechnicianId.ToString(),
+                    Selected = false
+                };
+            });
+
+            
+            model.customers = customers;
+            model.products = products;
+            model.technicians = technicians;
+            model.Action = "Edit";
+            if (ModelState.IsValid)
+            {   
+                if (model.Action == "Add")
+                {
+                    context.Incidents.Add(model.currentIncident);
                 }
                 else
                 {
-                    context.Incidents.Update(incident);
+                    context.Incidents.Update(model.currentIncident);
                 }
                 context.SaveChanges();
+
                 return RedirectToAction("List", "Incident");
             }
-
-            else
-            {
-                ViewBag.Action = action;
-                ViewBag.Customers = context.Customers.OrderBy(c => c.FirstName).ToList();
-                ViewBag.Products = context.Product.OrderBy(p => p.Name).ToList();
-                ViewBag.Technicians = context.Technicians.OrderBy(t => t.Name).ToList();
-                return View(incident);
-            }
+                return View("Edit", model);
 
         }
+
         [HttpGet]
         public IActionResult Delete(int id)
         {
