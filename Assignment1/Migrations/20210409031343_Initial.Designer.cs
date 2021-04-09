fd35d06279d0d130e646a7855144d844bc0b6081
Adam Vandyke <adam.s.vandyke@gmail.com>
2021-04-09 01:14:28 -0400

    Validation
    Upload from #Tracking on discord.

--- Assignment1/Migrations/20210409031343_Initial.Designer.cs
+++ Assignment1/Migrations/20210409031343_Initial.Designer.cs
@@ -4,14 +4,16 @@ using Assignment1.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Assignment1.Migrations
 {
     [DbContext(typeof(SportingContext))]
-    partial class SportingContextModelSnapshot : ModelSnapshot
+    [Migration("20210409031343_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Assignment1/Migrations/20210409031343_Initial.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Assignment1.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    CountryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.CountryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    YearlyPrice = table.Column<double>(type: "float", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Technicians",
+                columns: table => new
+                {
+                    TechnicianId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Technicians", x => x.TechnicianId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostalCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CountryId = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                    table.ForeignKey(
+                        name: "FK_Customers_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "CountryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Incidents",
+                columns: table => new
+                {
+                    IncidentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TechnicianId = table.Column<int>(type: "int", nullable: true),
+                    DateOpened = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateClosed = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Incidents", x => x.IncidentId);
+                    table.ForeignKey(
+                        name: "FK_Incidents_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Incidents_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Incidents_Technicians_TechnicianId",
+                        column: x => x.TechnicianId,
+                        principalTable: "Technicians",
+                        principalColumn: "TechnicianId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "CountryId", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Canada" },
+                    { 2, "US" },
+                    { 3, "Mexico" },
+                    { 4, "Other" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "ProductId", "Code", "Name", "ReleaseDate", "YearlyPrice" },
+                values: new object[,]
+                {
+                    { 1, "TRNY10", "Tournament Master", new DateTime(2021, 4, 8, 23, 13, 43, 245, DateTimeKind.Local).AddTicks(8979), 10.99 },
+                    { 2, "DRAFT10", "Draft Manager 2.0", new DateTime(2021, 4, 8, 23, 13, 43, 248, DateTimeKind.Local).AddTicks(2603), 5.9900000000000002 },
+                    { 3, "TRNY20", "Tournament Master 2.0", new DateTime(2021, 4, 8, 23, 13, 43, 248, DateTimeKind.Local).AddTicks(2639), 12.99 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Technicians",
+                columns: new[] { "TechnicianId", "Email", "Name", "Phone" },
+                values: new object[,]
+                {
+                    { 1, "fGallager@sporting.com", "Fark Gallager", "416-123-1234" },
+                    { 2, "tBurt@sporting.com", "Tim Burt", "647-267-3795" },
+                    { 3, "ePoe@sporting.com", "Edgar Poe", "437-769-1793" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerId", "Address", "City", "CountryId", "Email", "FirstName", "LastName", "Phone", "PostalCode", "State" },
+                values: new object[] { 1, "123 Random Lane", "Toronto", 1, "bruce@domain.com", "Bruce", "Wayne", "416-123-6456", "M4N 250", "Ontario" });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerId", "Address", "City", "CountryId", "Email", "FirstName", "LastName", "Phone", "PostalCode", "State" },
+                values: new object[] { 3, "123 Random Lane", "Toronto", 1, "wats@domain.com", "Emily", "Wats", "647-860-1678", "M4N 250", "Ontario" });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerId", "Address", "City", "CountryId", "Email", "FirstName", "LastName", "Phone", "PostalCode", "State" },
+                values: new object[] { 2, "123 Random Lane", "New York", 2, "janedoe@domain.com", "Jane", "Doe", "416-345-1237", "1234", "New York" });
+
+            migrationBuilder.InsertData(
+                table: "Incidents",
+                columns: new[] { "IncidentId", "CustomerId", "DateClosed", "DateOpened", "Description", "ProductId", "TechnicianId", "Title" },
+                values: new object[] { 3, 1, null, new DateTime(2021, 4, 8, 23, 13, 43, 248, DateTimeKind.Local).AddTicks(6578), "Unknown error when attempting to open program", 1, 3, "Error while attempting to run program" });
+
+            migrationBuilder.InsertData(
+                table: "Incidents",
+                columns: new[] { "IncidentId", "CustomerId", "DateClosed", "DateOpened", "Description", "ProductId", "TechnicianId", "Title" },
+                values: new object[] { 2, 3, null, new DateTime(2021, 4, 8, 23, 13, 43, 248, DateTimeKind.Local).AddTicks(6556), "Encountered compatability issues", 2, 3, "Error launching program" });
+
+            migrationBuilder.InsertData(
+                table: "Incidents",
+                columns: new[] { "IncidentId", "CustomerId", "DateClosed", "DateOpened", "Description", "ProductId", "TechnicianId", "Title" },
+                values: new object[] { 1, 2, null, new DateTime(2021, 4, 8, 23, 13, 43, 248, DateTimeKind.Local).AddTicks(5921), "Customer is having issues with installation on Win 10", 1, 3, "Could not install" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_CountryId",
+                table: "Customers",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Incidents_CustomerId",
+                table: "Incidents",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Incidents_ProductId",
+                table: "Incidents",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Incidents_TechnicianId",
+                table: "Incidents",
+                column: "TechnicianId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Incidents");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Technicians");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
